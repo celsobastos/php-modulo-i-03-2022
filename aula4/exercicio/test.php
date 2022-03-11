7fa01f61a511dbd65f196e2de1967c5ac58f6493
Celso Bastos <celsorico@gmail.com>
2022-03-10 22:37:31 -0300

    functions

--- aula4/exercicio/test.php
+++ aula4/exercicio/test.php
@@ -5,8 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicios</title>
+    <style>
+        .test { 
+            font-size: 50px; 
+            border: 1px solid black;
+            width: 100px;
+            height: 100px;
+            padding: 20px 0 0 20px;
+        }
+    </style>
 </head>
 <body>
-    
+    <!--  -->
+    <h1>Sorteio</h1>
+    <div class="test">
+        2
+    </div>
+    <a href="sorteio.php">Clique para sortear um numero.</a>
 </body>
 </html>
